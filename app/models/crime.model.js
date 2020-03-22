2820d6a9ed9583be5aee5e4c3081d9e1e5513155
paulo-manzone <pmanzonemaia@gmail.com>
2020-03-22 20:06:01 -0300

    GET Crimes working

--- app/models/crime.model.js
+++ app/models/crime.model.js
@@ -59,7 +59,7 @@ Crime.create = (newCrime, result) => {
   //================================================================================================
   //Reading a crime
   //================================================================================================
-  Crime.findById = async (crimeId, result) => {
+  Crime.findById = (crimeId, result) => {
 
     //Local variables storing every list of objects returned
     lCrime = null;
@@ -74,99 +74,86 @@ Crime.create = (newCrime, result) => {
         result(err, null);
         return;
       }
-      
   
       if (res.length) {
         console.log("found crime: ", res[0]);
-        //result(null, res[0]);
         lCrime = res[0];
-        return;
-      }
-  
-      //if not found
-      result({ kind: "not_found" }, null);
-    });
+      }else
+        //if not found
+        result({ kind: "not_found" }, null);
 
-    //Getting info about victims
-    sql.query(`select v.id_victim, v.tx_name from  crime as c 
+      //Getting info about victims
+      sql.query(`select v.id_victim, v.tx_name from  crime as c 
       inner join victim_crime as vc on vc.id_crime = c.id_crime
       inner join victim as v on vc.id_victim = v.id_victim
       where c.id_crime = ${crimeId}`, (err, res) => {
 
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-      
-  
-      if (res.length) {
-        console.log("found victim: ", res[0]);
-        //result(null, res[0]);
-        lVictims = res;
-        return;
-      }
-  
-      //if not found
-      result({ kind: "not_found" }, null);
-    });
+        console.log('3');
+        if (err) {
+          console.log("error: ", err);
+          result(err, null);
+          return;
+        }
 
-    //Getting info about weapons
-    sql.query(`select w.tx_model, wt.tx_weapon_type from crime as c
-    inner join weapon_crime as wc on wc.id_crime = c.id_crime
-      inner join weapon as w on w.id_weapon = wc.id_weapon
-      left join weapon_type as wt on wt.id_weapon_type = w.id_weapon_type
-      where c.id_crime = ${crimeId}`, (err, res) => {
+        if (res.length) {
+          console.log("found victim: ", res[0]);
+          lVictims = res;
+        }else
+          //if not found
+          result({ kind: "not_found" }, null);
+
+        //Getting info about weapons
+        sql.query(`select w.tx_model, wt.tx_weapon_type from crime as c
+        inner join weapon_crime as wc on wc.id_crime = c.id_crime
+          inner join weapon as w on w.id_weapon = wc.id_weapon
+          left join weapon_type as wt on wt.id_weapon_type = w.id_weapon_type
+          where c.id_crime = ${crimeId}`, (err, res) => {
+            
+          if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+          }
+                
+          if (res.length) {
+            console.log("found weapon: ", res[0]);
+            lWeapons = res;
+           
+          }else
+            //if not found
+            result({ kind: "not_found" }, null);
+
+          //Getting info about criminals
+          sql.query(`select cl.tx_name, ct.tx_type from crime as c
+          inner join criminal_crime as cc on cc.id_crime = c.id_crime
+            inner join criminal as cl on cl.id_criminal = cc.id_criminal
+            left join crime_type as ct on ct.id_crime_type = cc.id_crime_type
+            where c.id_crime = ${crimeId}`, (err, res) => {
+              
+            if (err) {
+              console.log("error: ", err);
+              result(err, null);
+              return;
+            }
+            
+            if (res.length) {
+              console.log("found weapon: ", res[0]);
+              lCriminals = res;
+            }else
+              //if not found
+              result({ kind: "not_found" }, null);
+
+            result (null, {'crime'  : lCrime,
+                   'victims': lVictims,
+                   'weapons': lWeapons,
+                   'criminals': lCriminals});
+          });
         
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-      
-  
-      if (res.length) {
-        console.log("found weapon: ", res[0]);
-        //result(null, res[0]);
-        lWeapons = res;
-        return;
-      }
-  
-      //if not found
-      result({ kind: "not_found" }, null);
-    });
+        });
 
-    //Getting info about criminals
-    sql.query(`select cl.tx_name, ct.tx_type from crime as c
-    inner join criminal_crime as cc on cc.id_crime = c.id_crime
-      inner join criminal as cl on cl.id_criminal = cc.id_criminal
-      left join crime_type as ct on ct.id_crime_type = cc.id_crime_type
-      where c.id_crime = ${crimeId}`, (err, res) => {
-        
-      if (err) {
-        console.log("error: ", err);
-        result(err, null);
-        return;
-      }
-      
-  
-      if (res.length) {
-        console.log("found weapon: ", res[0]);
-        //result(null, res[0]);
-        lCriminals = res;
-        console.log('rodou:', lCriminals);
-        return;
-      }
-  
-      //if not found
-      result({ kind: "not_found" }, null);
-    });
+      });
 
-    console.log('Resultado final:', lCrime, lVictims, lWeapons, lCriminals);
-    result (null, {'crime'  : await lCrime,
-                   'victims': await lVictims,
-                   'weapons': await lWeapons,
-                   'criminals': await lCriminals});
+    });
   };
 
   //================================================================================================
